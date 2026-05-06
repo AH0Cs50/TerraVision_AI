e63d9a0bb4d284353f35d2256b8c50d2f25aa0c1
Ahmed Habeeb <ah0cs50@gmail.com>
2026-05-06 06:05:11 +0300

    create test for weather service

--- Backend/shared/container.js
+++ Backend/shared/container.js
@@ -5,6 +5,7 @@ import PasswordHasher from '../service/common/passHash.service.js';
 //domain 
 import UserService from '../service/user.service.js'
 import AuthService from '../service/auth.service.js';
+import WeatherService from '../service/weather.service.js';
 
 
 //ensure dependency injection
@@ -14,5 +15,6 @@ export const passHasher = new PasswordHasher();
 
 //domain services
 export const userService = new UserService();
-export const authService = new AuthService(tokenService,userService);
+export const authService = new AuthService(tokenService,userService,passHasher);
+export const weatherService = new WeatherService();
 
